592c08064c242b85022636677a2f327a2e1bdddc
Scott <sfrancis@misinteractive.com>
2015-12-01 18:49:21 -0500

    Added bower.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,29 @@
 
 var gulp=require('gulp');
 var sassdoc = require('sassdoc');
+var sass = require('gulp-sass');
+var REPO_NAME='sass-visibility';
 
 gulp.task('default',function(){
-    console.log('echo sass visibility...');
+    console.log(REPO_NAME + ' ..."tasks: gulp dist|sassdoc"');
 });
+
 
 gulp.task('sassdoc', function () {
     return gulp.src('stylesheets/**/*.scss')
         .pipe(sassdoc());
 
 });
+
+
+gulp.task('dist', function () {
+    compileSass();
+});
+
+
+///private
+function compileSass(){
+    gulp.src('./src/visibility.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./dist'));
+}
